affe055620a853ea152609fa8ab910241f3b63d0
Akhil Bandaru <akhilbandaru326@gmail.com>
2026-06-03 22:13:09 +0530

    Prepare for deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-zmwmqxxj6rh-m5o^--1irzxao=8qb1m!%1er)u06k6o#=cvjl_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
